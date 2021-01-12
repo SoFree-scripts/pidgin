37a1a6ae7ae0039a7c37c627d8754f805d349e82
SoFree-scripts <76851715+SoFree-scripts@users.noreply.github.com>
2021-01-12 22:51:04 +0000

    Add files via upload
    Input text bar now at the bottom and centered.
    You can no longer type inside the 'console' area, only in the text input

--- im.py
+++ im.py
@@ -4,24 +4,28 @@ import os
 import time
 import threading 
 import webbrowser
+#os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 #import pygame
 log = os.path.join(".","sof.log")
 func = os.path.join(".","sofplus/data/pidgin.cfg")
+#pygame.mixer.pre_init(frequency=44100, size=-16, channels=1, buffer=512)
+#pygame.mixer.init()
 
 window = Tk()
-
+window.title("SoF console")
 messages = Text(window)
 messages.pack()
 
-scroll_y = Scrollbar(window, orient="vertical", command=messages.yview)
-scroll_y.pack(side="left", expand=True, fill="y")
 
-messages.configure(yscrollcommand=scroll_y.set)
 
 
 input_user = StringVar()
 input_field = Entry(window, text=input_user)
-input_field.pack(side=BOTTOM, fill=X)
+input_field.pack(side="bottom", fill=X,expand=True)
+scroll_y = Scrollbar(window, orient="vertical", command=messages.yview)
+scroll_y.pack(side="right", expand=True, fill="y")
+
+messages.configure(yscrollcommand=scroll_y.set)
 
 sizeOrig = os.path.getsize(log)
 with open("seekdata", "w") as f:
@@ -45,6 +49,7 @@ def checkUpdateLoop():
 				#print all lines to widget
 				if ": [" in x:
 					insertMe = ""
+					messages.configure(state="normal")
 					for s in x.split():
 						if "http" not in s:
 							insertMe += (str(s) + " ")
@@ -58,15 +63,14 @@ def checkUpdateLoop():
 							insertMe = " "
 					insertMe += "\n"
 					messages.insert(END, '%s' % insertMe)
-					#pygame.mixer.music.load("pop.mp3") #Loading File Into Mixer
-    				#pygame.mixer.music.play() #Playing It In The Whole Device
+					window.title("Unread")
+					#beep=pygame.mixer.Sound("hitmarker.wav") #Loading File Into Mixer
+					#beep.set_volume(0.02)
+					#beep.play() #Playing It In The Whole Device
 					if float(scroll_y.get()[1]) > 0.9:
 						messages.see("end")
+					messages.configure(state="disable")
 		time.sleep(1)
-
-def urlClick(e,url):
-	print(url)
-	webbrowser.open_new(url)
 
 def urlHandler(url):
 	#we have a httplink
@@ -80,18 +84,19 @@ def callback(event, tag):
     #print(event.widget.get('%s.first'%tag, '%s.last'%tag))
 
 def Enter_pressed(event):
-    input_get = input_field.get()
-    messages.insert(INSERT, '%s\n' % input_get)
-    # label = Label(window, text=input_get)
-    messages.see("end")
-    
-    #send the line to SoFplus
-    with open(func, "w") as f:
-    	input_get = input_get.replace("\"", "'")
-    	say = "set msgString \"" + input_get + "\""
-    	f.write(say)
-    input_user.set('')
-    return "break"
+	window.title("SoF Console")
+	input_get = input_field.get()
+	#messages.insert(INSERT, '%s\n' % input_get)
+	# label = Label(window, text=input_get)
+	messages.see("end")
+
+	#send the line to SoFplus
+	with open(func, "w") as f:
+		input_get = input_get.replace("\"", "'")
+		say = "set msgString \"" + input_get + "\""
+		f.write(say)
+	input_user.set('')
+	return "break"
 
 
 frame = Frame(window)  # , width=300, height=300)
